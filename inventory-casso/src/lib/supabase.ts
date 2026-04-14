243154843e57e3861e941883a0a354efd8176fa7
yansanity1998 <byanyan1998@gmail.com>
2026-04-14 14:55:23 +0800

    dsadasda

--- inventory-casso/src/lib/supabase.ts
+++ inventory-casso/src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+const supabaseUrl = 'https://rdcvhakfsiftkgqvtxmu.supabase.co'
+const supabaseAnonKey = 'sb_publishable_31515Z0qZI2IX6mq56HVMA_8XwaNYyn'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
